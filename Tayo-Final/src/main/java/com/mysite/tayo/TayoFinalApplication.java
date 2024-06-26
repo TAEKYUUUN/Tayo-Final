c1f6fd44ecd4d2fdbe1b9f6e0d55dc2888988dda
TAEKYUN WOO <wootaekyun10@gmail.com>
2024-06-26 10:21:27 +0900

    Tae-kyun First Commit

---
+++ Tayo-Final/src/main/java/com/mysite/tayo/TayoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.tayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TayoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TayoFinalApplication.class, args);
+	}
+
+}
