922d537b867e5b27c3c8d239ab835fe88e5f2057
TAEKYUN WOO <wootaekyun10@gmail.com>
2024-06-27 14:59:18 +0900

    ttt

--- src/main/java/com/mysite/tayo/repository/VoteItemRepository.java
+++ src/main/java/com/mysite/tayo/repository/VoteItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mysite.tayo.entity.VoteItem;
 
 public interface VoteItemRepository extends JpaRepository<VoteItem, Long>{
-
+// wntjr
 }
