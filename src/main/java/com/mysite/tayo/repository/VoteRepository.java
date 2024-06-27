e53adc8abbfd7e01c1e1f68ab8ee0d9b73871a5d
TAEKYUN WOO <wootaekyun10@gmail.com>
2024-06-27 14:48:56 +0900

    Merge branch 'main' of https://github.com/TAEKYUUUN/Tayo-Final.git into main

--- src/main/java/com/mysite/tayo/repository/VoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.tayo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysite.tayo.entity.Vote;
-
-public interface VoteRepository extends JpaRepository<Vote, Long>{
-	// 아무거나 주석
-}
