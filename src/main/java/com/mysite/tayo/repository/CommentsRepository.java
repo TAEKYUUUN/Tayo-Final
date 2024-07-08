2247d5fa7a7cb9c3d71cddae185b75654eb9f5f7
TAEKYUN WOO <wootaekyun10@gmail.com>
2024-07-08 16:38:59 +0900

    TaeKyun Commit 0708 pm16:38

--- src/main/java/com/mysite/tayo/repository/CommentsRepository.java
+++ src/main/java/com/mysite/tayo/repository/CommentsRepository.java
@@ -1,11 +1,14 @@
 package com.mysite.tayo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mysite.tayo.entity.Comments;
+import com.mysite.tayo.entity.Post;
 
 @Repository
 public interface CommentsRepository extends JpaRepository<Comments, Long>{
-
+	List<Comments> findByPost(Post post);
 }
