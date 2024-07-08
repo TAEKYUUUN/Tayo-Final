223da0c58674c20c69ee66b60acd9e4749e2d7a9
김민성 <@Minseong>
2024-07-08 16:40:53 +0900

    minseong 0708 1640

--- src/main/java/com/mysite/tayo/repository/AlarmRepository.java
+++ src/main/java/com/mysite/tayo/repository/AlarmRepository.java
@@ -1,11 +1,19 @@
 package com.mysite.tayo.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mysite.tayo.entity.Alarm;
+import com.mysite.tayo.entity.Member;
 
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long>{
-
+	
+	@Query("SELECT a FROM Alarm a WHERE a.member = :member")
+	List<Alarm> findByMemberMemberIdx(@Param("member") Member member, Sort sort);
 }
